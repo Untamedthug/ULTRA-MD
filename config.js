24f32507fddf25981d20eeff8d6fc7ae1ad23fd8
𝘿𝘼𝙍𝙆 𝙋𝙍𝙄𝙉𝘾𝙀 <156942654+Untamedthug@users.noreply.github.com>
2024-11-16 06:42:22 +0300

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const defaultOwner = '254757837444';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS = 'your number';  // put your number here
+const ownervb = process.env.OWNERS = '254757837444';  // put your number here
 
 const ownerlist = ownervb.split(';');
 
